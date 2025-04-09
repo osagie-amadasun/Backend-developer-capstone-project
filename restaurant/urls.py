aa4eb5ab6b07bb57d25108e68c9e4079cae39dea
Osagie Amadasun <osagieamadasun123@gmail.com>
2025-04-10 00:56:56 +0100

    i don't even know what i just committed here lowkey...but i'm committing regardless XD

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    
     path('home', views.index, name='home'),
     path('items/', views.MenuItemView.as_view(), name='items'),
     path('items/<int:pk>/', views.SingleMenuItemView.as_view(), name='single_menu'),
